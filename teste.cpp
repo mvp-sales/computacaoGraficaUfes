6f1ea853c147f0f5c092ab836d7cd263fcec8b8b
theMxyzptlk <old.marcussales@gmail.com>
2016-09-02 16:55:43 -0300

    finished implementation

--- teste.cpp
+++ teste.cpp
@@ -17,108 +17,21 @@ struct Quadrado{
 
 // VARIÁVEIS GLOBAIS
 int largura,altura,fundoR,fundoG,fundoB;
-int squareSize,corR,corG,corB;
 Quadrado quad;
 const char* titulo;
 
 bool estadoInsercao=true,estadoAlteracao=false;
 bool left_button_pressed = false;
+bool drag = false;
 
+bool isSquareArea(int x,int y);
+void drawSquare();
+void mouseClick(int button,int state,int x,int y);
+void dragAndDrop(int x,int y);
+void display();
+void init(void);
 void processaJanela(const XMLNode* node);
 void processaQuadrado(const XMLNode* node);
-
-bool isSquareArea(int x,int y){
-	if((abs(quad.centroX - x) <= quad.tamanho/2.0) && (abs(quad.centroY - y) <= quad.tamanho/2.0)){
-		return true;
-	}
-	return false;
-}
-
-void drawSquare(int x,int y){
-	quad.centroY = y;
-	quad.centroX = x;
-	
-	float left = x-quad.tamanho/2.0,
-		right = x+quad.tamanho/2.0,
-		bottom = y+quad.tamanho/2.0,
-		top = y- quad.tamanho/2.0;
-	
-
-	glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-		glColor3f(quad.corR,quad.corG,quad.corB);
-		glVertex3f(left, altura-top,0.0);    // x, y
-		glVertex3f( right, altura-top,0.0);
-		glVertex3f( right,  altura-bottom,0.0);
-		glVertex3f(left,altura-bottom,0.0);
-
-
-	  //cout<<"coord x:"<<x<<"\ncoord y:"<<y<<"\n";
-	glEnd();
-	glFlush();
-}
-
-void mouseClick(int button,int state,int x,int y){
-	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && estadoInsercao){
-
-		/*quad.centroY = y;
-		quad.centroX = x;
-		
-		float left = x-quad.tamanho/2.0,
-			right = x+quad.tamanho/2.0,
-			bottom = y+quad.tamanho/2.0,
-			top = y- quad.tamanho/2.0;
-		
-
-		glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-			glColor3f(quad.corR,quad.corG,quad.corB);
-			glVertex3f(left, altura-top,0.0);    // x, y
-			glVertex3f( right, altura-top,0.0);
-			glVertex3f( right,  altura-bottom,0.0);
-			glVertex3f(left,altura-bottom,0.0);
-
-
-		  //cout<<"coord x:"<<x<<"\ncoord y:"<<y<<"\n";
-		glEnd();
-		glFlush();*/
-
-		drawSquare(x,y);
-
-		estadoInsercao = false;
-		estadoAlteracao = true;
-	}else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN && !estadoInsercao){
-		//cout<<"coordir x:"<<x<<"\ncoordir y:"<<y<<"\n";
-		if(isSquareArea(x,y)){
-			glutPostRedisplay();
-			estadoInsercao = true;
-			estadoAlteracao = false;
-		}
-	}else if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && estadoAlteracao){
-		left_button_pressed = true;
-	}else{
-		left_button_pressed = false;
-	}
-}
-
-void dragAndDrop(int x,int y){
-	if(left_button_pressed && isSquareArea(x,y)){
-		glutPostRedisplay();
-		drawSquare(x,y);
-	}
-}
-
-void display() {
-   glClearColor(fundoR,fundoG,fundoB,0); // Set background color to black and opaque
-   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
-   glFlush();  // Render now
-}
-
-void init(void){
-	glClearColor(0.0,0.0,0.0,0.0);
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0,largura,0.0,altura,-1.0,1.0);
-}
 
 /* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
@@ -138,7 +51,7 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);                 // Initialize GLUT
 	glutCreateWindow(titulo); // Create a window with the given title
-	//glutInitWindowSize(widthWindow, lengthWindow);   // Set the window's initial width & height
+
 	init();
 
 	glutMotionFunc(dragAndDrop);
@@ -147,6 +60,81 @@ int main(int argc, char** argv) {
 
 	glutMainLoop();           // Enter the infinitely event-processing loop
 	return 0;
+}
+
+bool isSquareArea(int x,int y){
+	if((abs(quad.centroX - x) <= quad.tamanho/2.0) && (abs(quad.centroY - y) <= quad.tamanho/2.0)){
+		return true;
+	}
+	return false;
+}
+
+void drawSquare(){
+
+	float left = quad.centroX-quad.tamanho/2.0,
+	 right = quad.centroX+quad.tamanho/2.0,
+	 bottom = quad.centroY+quad.tamanho/2.0,
+	 top = quad.centroY- quad.tamanho/2.0;
+
+
+ glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+	 glColor3f(quad.corR,quad.corG,quad.corB);
+	 glVertex3f(left, altura-top,0.0);    // x, y
+	 glVertex3f( right, altura-top,0.0);
+	 glVertex3f( right,  altura-bottom,0.0);
+	 glVertex3f(left,altura-bottom,0.0);
+ 	glEnd();
+}
+
+void mouseClick(int button,int state,int x,int y){
+	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && estadoInsercao){
+
+		quad.centroY = y;
+		quad.centroX = x;
+
+		drawSquare();
+		glFlush();
+
+		estadoInsercao = false;
+		estadoAlteracao = true;
+	}else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN && !estadoInsercao){
+		if(isSquareArea(x,y)){
+			glutPostRedisplay();
+			estadoInsercao = true;
+			estadoAlteracao = false;
+		}
+	}else if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && estadoAlteracao){
+		left_button_pressed = true;
+	}else{
+		left_button_pressed = false;
+	}
+}
+
+void dragAndDrop(int x,int y){
+	if(left_button_pressed && isSquareArea(x,y)){
+		quad.centroX = x;
+		quad.centroY = y;
+		drag = true;
+		glutPostRedisplay();
+	}
+}
+
+void display() {
+   glClearColor(fundoR,fundoG,fundoB,0); // Set background color to black and opaque
+   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+	 if(drag){
+		drawSquare();
+		drag = false;
+	 }
+   glFlush();  // Render now
+}
+
+void init(void){
+	glClearColor(0.0,0.0,0.0,0.0);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0,largura,0.0,altura,-1.0,1.0);
 }
 
 void processaJanela(const XMLNode* node){
@@ -168,14 +156,10 @@ void processaJanela(const XMLNode* node){
 
 void processaQuadrado(const XMLNode* node){
 	const XMLElement* elem = node->ToElement();
-	/*elem->QueryIntAttribute("tamanho",&squareSize);
-	elem->QueryIntAttribute("corR",&squareR);
-	elem->QueryIntAttribute("corG",&squareG);
-	elem->QueryIntAttribute("corB",&squareB);*/
-	
+
 	elem->QueryIntAttribute("tamanho",&quad.tamanho);
 	elem->QueryIntAttribute("corR",&quad.corR);
 	elem->QueryIntAttribute("corG",&quad.corG);
 	elem->QueryIntAttribute("corB",&quad.corB);
-	
+
 }
